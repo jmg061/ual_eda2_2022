123c4726cd95c54321884cd1efc8ff4e5dbc506e
amt871 <amt871@inlumine.ual.es>
2022-03-07 12:59:31 +0100

    Primer DYV

--- practica_01/source/Practica01/src/practica01/DyV.java
+++ practica_01/source/Practica01/src/practica01/DyV.java
@@ -9,43 +9,45 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 public class DyV {
-	
+
 	ArrayList<Jugador> datos;
-	
+
 	public DyV() {
-		
-		this.datos=new ArrayList<>();
-		
+
+		this.datos = new ArrayList<>();
+
 	}
-	
+
 	public void load() {
-		
-		BufferedReader br=null;
+
+		BufferedReader br = null;
 		String line;
 		String[] items;
-		Jugador aux=null;
-		
+		Jugador aux = null;
+
 		try {
-			br=new BufferedReader(new FileReader("src"+File.separator+"practica01"+File.separator+"NbaStats.csv"));
+			br = new BufferedReader(
+					new FileReader("src" + File.separator + "practica01" + File.separator + "NbaStats.csv"));
 			while ((line = br.readLine()) != null) {
-				if(line.contains("#")) continue;
-				items=line.split(";");
-				aux=new Jugador(items[2],items[6],items[4], Integer.parseInt(items[8]));
-				if(!this.datos.contains(aux))
-				//System.out.println(this.datos.contains(aux));
+				if (line.contains("#"))
+					continue;
+				items = line.split(";");
+				aux = new Jugador(items[2], items[6], items[4], Integer.parseInt(items[8]));
+				if (!this.datos.contains(aux))
+					// System.out.println(this.datos.contains(aux));
 					this.datos.add(aux);
 				else {
-					
-					int pos=this.datos.indexOf(aux);
-					int score=(this.datos.get(pos).getScore()+Integer.parseInt(items[8]))/2;
+
+					int pos = this.datos.indexOf(aux);
+					int score = (this.datos.get(pos).getScore() + Integer.parseInt(items[8])) / 2;
 					this.datos.get(pos).setScore(score);
-					if(!this.datos.get(pos).getPositions().contains(items[4]))
-					this.datos.get(pos).getPositions().add(items[4]);
-					if(!this.datos.get(pos).getTeams().contains(items[6]))
-					this.datos.get(pos).getTeams().add(items[6]);
+					if (!this.datos.get(pos).getPositions().contains(items[4]))
+						this.datos.get(pos).getPositions().add(items[4]);
+					if (!this.datos.get(pos).getTeams().contains(items[6]))
+						this.datos.get(pos).getTeams().add(items[6]);
 				}
-				//for(String word : items) System.out.println(word);
-				
+				// for(String word : items) System.out.println(word);
+
 			}
 			br.close();
 		} catch (FileNotFoundException e) {
@@ -55,47 +57,47 @@ public class DyV {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
+
 	}
-	
-	public static void mergesort(Jugador A[], int izq, int der) {
-		if (izq > der) {
-			int medio = (int) Math.ceil((izq + der)/2);
+
+	public void mergesort(ArrayList<Jugador> A, int izq, int der) {
+		if (izq < der) {
+			int medio = (int) Math.ceil((izq + der) / 2);
 			mergesort(A, izq, medio);
 			mergesort(A, medio + 1, der);
 			merge(A, izq, medio, der);
-			
+
 		}
 	}
-	
-	public static void merge(Jugador A[] , int izq, int medio, int der) {
+
+	public void merge(ArrayList<Jugador> a, int izq, int medio, int der) {
 		int i, j;
-		Jugador[] aux = new Jugador[A.length];
-		for (int l = 0; l < aux.length; l++) {
-			aux[l] = A[l];
+		ArrayList<Jugador>  aux = new ArrayList<Jugador>();
+		for (Jugador jugador : aux) {
+			aux.add(jugador);
 		}
-		
+
 		i = izq;
 		j = medio + 1;
-		
+
 		while (izq <= medio && j <= der) {
-			if (A[izq].getScore() > A[j].getScore()) {
-				aux[i++] = A[izq++];
-			}else {
-				aux[i++] = A[j++];
+			if (a.get(izq).getScore() > a.get(j).getScore()) {
+				a.set(i++, aux.get(izq++));
+			} else {
+				a.set(i++, aux.get(j++));
 			}
 		}
-		
+
 		while (izq < medio) {
-			aux[i++] = A[izq ++];
-			
+			a.set(i++, aux.get(izq++));
+
 		}
 		while (j < der) {
-			aux[i++] = A[j++];
+			a.set(i++, aux.get(j++));
 		}
 		
+		
+
 	}
 
 	public ArrayList<Jugador> getDatos() {
--- practica_01/source/Practica01/src/practica01/Main.java
+++ practica_01/source/Practica01/src/practica01/Main.java
@@ -1,5 +1,8 @@
 package practica01;
 
+import java.util.Collection;
+import java.util.Collections;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -7,9 +10,12 @@ public class Main {
 
 		DyV jugadores = new DyV();
 		jugadores.load();
+		jugadores.mergesort(jugadores.datos, 0, jugadores.datos.size() - 1);
 		for(Jugador aux : jugadores.getDatos())
 			System.out.println(aux);
 		
+		
 	}
+	
 
 }
