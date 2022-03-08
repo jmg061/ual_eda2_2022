6de4127b3583de2489612625b68b447485e2aafc
Juan Raul Mellado Garcia <jmg061@inlumine.ual.es>
2022-03-08 13:24:48 +0100

    Update Main.java

--- practica_01/source/Practica01/src/practica01/Main.java
+++ practica_01/source/Practica01/src/practica01/Main.java
@@ -9,8 +9,10 @@ public class Main {
 		jugadores.load();
 		for(Jugador aux : jugadores.getDatos())
 			System.out.println(aux);
-		Jugador[] arr = new Jugador[jugadores.getDatos().size()];
-		jugadores.getDatos().toArray(arr);
+//		System.out.println(jugadores.getDatos().get(0));
+//		System.out.println(jugadores.getDatos().get(1));
+//		System.out.println(jugadores.getDatos().get(2));
+//	
 		
 	}
 
