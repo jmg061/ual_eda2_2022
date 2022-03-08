1913ec77e902c25b45b5739eb4f97d841851ae0e
Juan Raul Mellado Garcia <jmg061@inlumine.ual.es>
2022-03-08 13:26:58 +0100

    Update Main.java

--- practica_01/source/Practica01/src/practica01/Main.java
+++ practica_01/source/Practica01/src/practica01/Main.java
@@ -1,5 +1,8 @@
 package practica01;
 
+import java.util.Collection;
+import java.util.Collections;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -7,13 +10,17 @@ public class Main {
 
 		DyV jugadores = new DyV();
 		jugadores.load();
+		System.out.println(jugadores.datos.size());
+		jugadores.mergesort();
+		System.out.println(jugadores.datos.size());
 		for(Jugador aux : jugadores.getDatos())
 			System.out.println(aux);
 //		System.out.println(jugadores.getDatos().get(0));
 //		System.out.println(jugadores.getDatos().get(1));
 //		System.out.println(jugadores.getDatos().get(2));
-//	
+//		
 		
 	}
+	
 
 }
