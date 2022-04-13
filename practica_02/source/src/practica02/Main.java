12dcc2afa4606e8345230a2d9afb194131bfbff6
amt871 <amt871@inlumine.ual.es>
2022-04-13 12:09:58 +0200

    Lectura de tiempos

--- practica_02/source/src/practica02/Main.java
+++ practica_02/source/src/practica02/Main.java
@@ -9,30 +9,42 @@ public class Main {
 		Greedy greedy = new Greedy();
 		ArrayList<Pavimento> resultados = new ArrayList<Pavimento>();
 
-		greedy.load("graphEDAland.txt");
+		greedy.load("graphEDAlandLarge.txt");
 //		System.out.println(greedy.getNet());
 		
 		System.out.println();
 		System.out.println("ConexoConPQ");
 		greedy.InicializarNodos();
+		long inicio = System.currentTimeMillis();
 		resultados = greedy.ConexoBase();
-		for (Pavimento pavimento : resultados) {
-			System.out.println("Inicio: " + pavimento.getInicio() + " Fin: " + pavimento.getFin() + "  Coste:" + pavimento.getCoste());
-		}
+		long fin = System.currentTimeMillis();
+		double tiempo = (double) ((fin - inicio));
+		System.out.println("Tiempo => " + tiempo);
+//		for (Pavimento pavimento : resultados) {
+//			System.out.println("Inicio: " + pavimento.getInicio() + " Fin: " + pavimento.getFin() + "  Coste:" + pavimento.getCoste());
+//		}
 		System.out.println();
 		System.out.println("ConexoSinPQ");
 		greedy.InicializarNodos();
+		inicio = System.currentTimeMillis();
 		resultados = greedy.ConexoSinPQ();
-		for (Pavimento pavimento : resultados) {
-			System.out.println("Inicio: " + pavimento.getInicio() + " Fin: " + pavimento.getFin() + "  Coste:" + pavimento.getCoste());
-		}
+		fin = System.currentTimeMillis();
+		tiempo = (double) ((fin - inicio));
+		System.out.println("Tiempo => " + tiempo);
+//		for (Pavimento pavimento : resultados) {
+//			System.out.println("Inicio: " + pavimento.getInicio() + " Fin: " + pavimento.getFin() + "  Coste:" + pavimento.getCoste());
+//		}
 		System.out.println();
 		System.out.println("NoConexo");
 		greedy.InicializarNodos();
+		inicio = System.currentTimeMillis();
 		resultados = greedy.NoConexo();
-		for (Pavimento pavimento : resultados) {
-			System.out.println("Inicio: " + pavimento.getInicio() + " Fin: " + pavimento.getFin() + "  Coste:" + pavimento.getCoste());
-		}
+		fin = System.currentTimeMillis();
+		tiempo = (double) ((fin - inicio));
+		System.out.println("Tiempo => " + tiempo);
+//		for (Pavimento pavimento : resultados) {
+//			System.out.println("Inicio: " + pavimento.getInicio() + " Fin: " + pavimento.getFin() + "  Coste:" + pavimento.getCoste());
+//		}
 
 		greedy.generadorDeRedes(false, 100, 200);
 
